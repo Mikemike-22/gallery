77275a36a56225ebf049e4fa010bed323e767d44
Michael Korir <mikor22@gmail.com>
2019-06-17 05:51:06 +0300

    create model for location

--- photos/models.py
+++ photos/models.py
@@ -11,7 +11,7 @@ class Image(models.Models):
 
     def __str__(self):
         return self.image_name
-        
+
  class Category(models.Model):
      category = models.CharField(max_length=50)
 
@@ -23,3 +23,20 @@ class Image(models.Models):
 
     def delete_category(self):
         Category.objects.filter().delete()
+
+class Location(models.Model):
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.location
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        Location.objects.filter().delete()
+
+    @classmethod
+    def get_location(cls):
+        location_found = cls.objects.all()
+        return location_found
